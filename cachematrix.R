5d05869568f3803e478ce2552ddf1ea6d873fdd6
Prettyfield <bruce_beauchamp@hotmail.com>
2014-07-18 17:43:17 -0700

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  Returns the inverse of a matrix.  If the inverse has been calculated,
+##  the inverse is cached for later retrieval.
+##
+##  The inverse is only calculated if the original matrix x
+##  has changed and the inverse needs ot be recalculated
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(original_matrix = matrix()) {
+#**************************************************
+#  Creates a list containing functions which 
+#    1) sets the original matrix
+#    2) gets the original matrix
+#    3) sets the matrix inverse
+#    4) gets the matrix inverse
+#**************************************************
+  
+  inverse <- NULL
+  
+  set <- function(y) {
+    original_matrix <<- y
+    inverse <<- NULL
+  }
+  
+  get <- function() original_matrix
+  
+  setinverse <- function(inverse_in)  inverse <<- inverse_in
+  
+  getinverse <- function() inverse
+  
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(original_matrix, ...) {
+##############################################
+##  Return a matrix that is the inverse of 'original_matrix'
+##  But utilize a cache to only calculate the inverse if the 
+##############################################  
+
+  inverse <- original_matrix$getinverse()
+
+  if (!is.null(inverse)) {
+    
+    message("getting cached data")
+    return(inverse)
+  
+  }
+
+  matx <- original_matrix$get()
+
+  inverse <- solve(matx)
+
+  original_matrix$setinverse(inverse)
+
+  inverse
+  
 }
